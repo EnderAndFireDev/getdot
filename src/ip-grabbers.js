8db3e13007e362268ed61fff07709a29515b91e2
Ender <xenderpugx@gmail.com>
2019-06-09 08:59:42 +0100

    Update ip-grabbers.js

--- src/ip-grabbers.js
+++ src/ip-grabbers.js
@@ -1,21 +1,23 @@
 // Feel free to open a pull request. Add any domain which is linked to an IP grabber and provide proof it belongs to a IP tracking service.
 
-grabify.link
-bmwforum.co
-leancoding.co
-spottyfly.com
-stopify.co
-yoütu.be
-discörd.com
-minecräft.com
-freegiftcards.co
-disçordapp.com
-särahah.eu
-särahah.pl
-xda-developers.us
-quickmessage.us
-fortnight.space
-fortnitechat.site
-youshouldclick.us
-joinmy.site
-watches-my.stream
+[
+  "grabify.link",
+  "bmwforum.co",
+  "leancoding.co",
+  "spottyfly.com",
+  "stopify.co",
+  "yoütu.be",
+  "discörd.com",
+  "minecräft.com",
+  "freegiftcards.co",
+  "disçordapp.com",
+  "särahah.eu",
+  "särahah.pl",
+  "xda-developers.us",
+  "quickmessage.us",
+  "fortnight.space",
+  "fortnitechat.site",
+  "youshouldclick.us",
+  "joinmy.site",
+  "watches-my.stream"
+]
